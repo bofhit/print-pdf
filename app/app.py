359bedb100efc8476ff6ed83090c869419574b57
Tim <it@blessingsofhope.com>
2023-06-19 15:58:15 -0400

    Make pdf with var from flask form.

--- app/app.py
+++ app/app.py
@@ -3,12 +3,15 @@ import os
 
 from flask import Flask, redirect, render_template, request
 
+from make_pdf import make_pdf
+
 app = Flask(__name__)
 
 # Load variables from .env
 dotenv.load_dotenv()
 
 FLASK_PORT = os.environ['FLASK_PORT']
+PDF_OUTPATH = os.environ['PDF_OUTPATH']
 
 @app.route('/name-tag', methods=['GET', 'POST'])
 def print_name_tag():
@@ -18,6 +21,9 @@ def print_name_tag():
         return render_template('template.html')
 
     if request.method == 'POST':
+        name = request.form.get('name-tag-name')
+        print(name)
+        make_pdf('my_pdf.pdf', name=name)
         return redirect('/name-tag')
 
 if __name__ == "__main__":
