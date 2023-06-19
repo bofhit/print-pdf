175b2cff89c46b05ef35759866efcfa7fbe1de7a
Tim <it@blessingsofhope.com>
2023-06-19 13:42:15 -0400

    Added redirect to print_tag_name "POST".

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import dotenv
 import os
 
-from flask import Flask, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -10,12 +10,16 @@ dotenv.load_dotenv()
 
 FLASK_PORT = os.environ['FLASK_PORT']
 
-@app.route('/name-tag', methods=['POST'])
-def name-tag():
+@app.route('/name-tag', methods=['GET', 'POST'])
+def print_name_tag():
     """Print a name tag."""
-    print(request.form.get('name-tag'))
-    return "Hello World!"
-  
+
+    if request.method == 'GET':
+        return render_template('template.html')
+
+    if request.method == 'POST':
+        return redirect('/name-tag')
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=FLASK_PORT)
     
