afaf2d4e5ff95ab82933dc4e0c5b8b1f45b4aab8
Tim Burkholder <it@blessingsofhope.com>
2023-06-19 14:54:03 -0400

    Make a PDF with a variable.

--- app/make_pdf.py
+++ app/make_pdf.py
@@ -2,7 +2,7 @@ __doc__ = """Accept some parameter(s) and insert the in a pdf."""
 
 from fpdf import FPDF
 
-def create_pdf(pdf_path, **kwargs):
+def make_pdf(pdf_path, **kwargs):
     """ Create a pdf, with optional text variables."""
 
     name = kwargs['name']
@@ -11,9 +11,11 @@ def create_pdf(pdf_path, **kwargs):
 
     pdf.add_page()
     pdf.set_font('Arial')
-    pdf.set_font_size('18')
+    pdf.set_font_size(18)
     
-    pdf.cell(50, 50, )
+    pdf.cell(50, 50, 'Hello my name is', 0, 1, 'C')
 
+    pdf.cell(50, 25, name, 0, 1, 'C')
 
+    pdf.output(pdf_path)
 
